f7ce48b3088fc191f2446aa4a9257665ba2488ba
Filipe Colaquecez <filipe.colaquecez@sinqia.com.br>
2020-10-07 20:48:45 -0300

    Correção projeto

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import api from '../Services/api';
 
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 
 export default class Main extends Component {
-
-    render() {
-        return(
-            <View>
-                <Text>Página Main</Text>
-            </View>
-        );
-        
-    }
-    
+  render() {
+    return (
+      <View>
+        <Text>Página Main</Text>
+      </View>
+    );
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
-
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import Main from './pages/main';
 
-export default createAppContainer(createStackNavigator({
-  Main,
-}, {
-  navigationOptions: {
-    headerStyle: {
-      backgroundColor: '#DA552F',
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Main,
     },
-    headerTintColor: '#FFF',
-  },
-},
-));
+    {
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: '#DA552F',
+        },
+        headerTintColor: '#FFF',
+      },
+    },
+  ),
+);
